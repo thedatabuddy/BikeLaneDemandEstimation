953fe00e2f58e846dcb92d3902091c6c7e7a25b0
thedatabuddy <aadi22796@gmail.com>
2019-02-16 12:20:53 -0600

    added vizzes for WoE

--- score_optimization/woe_calculation.py
+++ score_optimization/woe_calculation.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 from tabulate import tabulate
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly
+
 
 pdtabulate=lambda df:tabulate(df,headers='keys',tablefmt='psql')
 
@@ -19,5 +23,8 @@ event_count=407
 for feature in features:
     df=pd.read_csv(r'E:\UMKCStudies\Thesis\score_optimization\feature_counts_merged\\'+feature+'.csv',index_col=0)
     df['WoEScore']=WoE(df['wolane'],non_event_count,df['wlane'],event_count)
+    df=df.sort_values('WoEScore')
+    data = [go.Bar(x=df.reset_index()['index'],y=df['WoEScore'])]
+    plotly.offline.plot(data, filename='graph_'+feature+'.html')
     df.to_csv(r'E:\UMKCStudies\Thesis\score_optimization\woe_calculated\\'+feature + '.csv')
 
